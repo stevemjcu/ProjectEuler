590570ce658b723b7aa6ec65ce694732647bbecd
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-08 23:05:00 -0400

    Solve problem 5

---
+++ ProjectEuler/Problem05.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectEuler
+{
+	internal class Problem05 : Problem<int>
+	{
+		public Problem05() => N = 20;
+
+		/// <returns>The smallest positive number whose factors include 1 through N.</returns>
+		public override int Solve()
+		{
+			for (int i = 1; ; i++)
+			{
+				if (Enumerable.Range(1, N).All(j => i % j == 0))
+				{
+					return i;
+				}
+			}
+		}
+	}
+}
