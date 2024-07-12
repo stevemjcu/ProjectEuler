c406fba7a69f31de67ae56828688ec8a7db89ce3
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-12 08:54:39 -0400

    Revise solution 7

--- ProjectEuler/Problem07.cs
+++ ProjectEuler/Problem07.cs
@@ -7,20 +7,11 @@
 		/// <returns>The Nth prime number.</returns>
 		public override int Solve()
 		{
-			return GetPrimes(N).Last();
-		}
-
-		/// <returns>A sequence that contains the prime numbers up to and including n.</returns>
-		public static IEnumerable<int> GetPrimes(int n)
-		{
-			for (var (i, j) = (1, 0); j < n; i++)
-			{
-				if (Problem03.IsPrime(i))
-				{
-					yield return i;
-					j++;
-				}
-			}
+			return Utilities
+				.InfiniteRange(1)
+				.Where(x => Problem03.IsPrime(x))
+				.Take(N)
+				.Last();
 		}
 	}
 }
--- ProjectEuler/Utilities.cs
+++ ProjectEuler/Utilities.cs
@@ -5,9 +5,18 @@
 		/// <returns>A sequence that contains a range of sequential integral numbers.</returns>
 		public static IEnumerable<long> LongRange(long start, long count)
 		{
-			for (long l = start; l <= start + count; l += 1L)
+			for (long l = start; l <= start + count; l++)
 			{
 				yield return l;
+			}
+		}
+
+		/// <returns>A sequence that contains an infinite range of sequential integral numbers.</returns>
+		public static IEnumerable<int> InfiniteRange(int start)
+		{
+			for (int i = start; ; i++)
+			{
+				yield return i;
 			}
 		}
 	}
