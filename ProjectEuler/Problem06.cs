5a345eb47cd92ff0c38802a14de38f7556f9ff39
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-11 22:23:55 -0400

    Solve problem 6

---
+++ ProjectEuler/Problem06.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectEuler
+{
+	internal class Problem06 : Problem<int>
+	{
+		public Problem06() => N = 100;
+
+		/// <returns>The difference between the sum of squares and the square of the sum for the first N natural numbers.</returns>
+		public override int Solve()
+		{
+			var numbers = Enumerable.Range(1, N);
+			var a = numbers.Select(x => x * x).Sum();
+			var b = numbers.Sum(); b *= b;
+			return b - a;
+		}
+	}
+}
