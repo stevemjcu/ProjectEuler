e80ba56da8ce55c0285d8be7d5c9989aeaecf5cf
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-13 12:47:10 -0400

    Add test project

---
+++ ProjectEuler.Tests/ProblemsTests.cs
@@ -0,0 +1,11 @@
+namespace ProjectEuler.Tests
+{
+	[TestClass]
+	public class ProblemsTests
+	{
+		internal static object[,] Solutions = {
+			{ typeof(Problem01), "1000", "233168" },
+			{ typeof(Problem02), "4000000", "4613732" }
+		};
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ProjectEuler;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ProjectEuler.Tests")]
 
 var problem = new Problem04();
 Console.WriteLine($"Solving problem {problem.Index}...");
