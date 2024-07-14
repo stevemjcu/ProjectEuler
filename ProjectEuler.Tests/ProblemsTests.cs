669e2fb1370bb747561e94af524f496953843252
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-14 02:03:59 -0400

    Solve problem 10

--- ProjectEuler.Tests/ProblemsTests.cs
+++ ProjectEuler.Tests/ProblemsTests.cs
@@ -12,7 +12,8 @@ namespace ProjectEuler.Tests
 			[typeof(Problems_00.Problem_06), "25164150"],
 			[typeof(Problems_00.Problem_07), "104743"],
 			[typeof(Problems_00.Problem_08), "23514624000"],
-			[typeof(Problems_00.Problem_09), "31875000"]
+			[typeof(Problems_00.Problem_09), "31875000"],
+			[typeof(Problems_00.Problem_10), "142913828922"],
 		];
 
 		[TestMethod]
---
+++ ProjectEuler/Problems_00/Problem_10.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectEuler.Problems_00
+{
+	internal class Problem_10 : Problem
+	{
+		public long N = 2000000;
+
+		public override string Solve() => SolveInternal().ToString();
+
+		/// <returns>The sum of all the primes below N.</returns>
+		public long SolveInternal()
+		{
+			return Utilities
+				.Range(1L, N - 1L)
+				.Where(Problem_03.IsPrime)
+				.Sum();
+		}
+	}
+}
