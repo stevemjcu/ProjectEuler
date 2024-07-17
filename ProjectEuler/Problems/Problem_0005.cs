6f5b4ecb599f4978bbeafa02b025c96f60b8040a
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-16 22:39:30 -0400

    Solve problem 5 faster with loop

--- ProjectEuler/Problems/Problem_0005.cs
+++ ProjectEuler/Problems/Problem_0005.cs
@@ -5,13 +5,29 @@
 		public int N = 20;
 
 		/// <returns>The smallest positive number whose factors include 1 through N.</returns>
-		public override string Solve()
+		public override string Solve() => SolveLoop().ToString();
+
+		public int SolveLinq()
+		{
+			return Enumerable
+				.Range(1, int.MaxValue)
+				.First(i => Enumerable.Range(1, N).All(j => i % j == 0));
+		}
+
+		public int SolveLoop()
 		{
 			for (int i = 1; ; i++)
 			{
-				if (Enumerable.Range(1, N).All(j => i % j == 0))
+				for (int j = 1; j <= N; j++)
 				{
-					return i.ToString();
+					if (i % j != 0)
+					{
+						break;
+					}
+					if (j == N)
+					{
+						return i;
+					}
 				}
 			}
 		}
