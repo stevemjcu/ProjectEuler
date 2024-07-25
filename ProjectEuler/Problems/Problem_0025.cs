4bae7355562125ec005793f86d89c8c18bbc1d3d
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-07-25 08:59:01 -0400

    Solve problem 25

---
+++ ProjectEuler/Problems/Problem_0025.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+	class Problem_0025 : Problem
+	{
+		public int N = 1000;
+
+		/// <returns>The index of the first Fibonacci term to have N digits.</returns>
+		public override object Solve()
+		{
+			return GetFibonacciSequence()
+				.Select((num, idx) => (num, idx))
+				.First(x => x.num.ToString().Length >= N)
+				.idx;
+		}
+
+		/// <returns>The terms of the Fibonacci sequence, starting at 1.</returns>
+		public static IEnumerable<BigInteger> GetFibonacciSequence()
+		{
+			var (a, b) = (BigInteger.One, BigInteger.One);
+			while (true)
+			{
+				yield return a;
+				(a, b) = (b, a + b);
+			}
+		}
+	}
+}
