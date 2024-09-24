509a871b45508a4e275b45683ec04bfebbe78487
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-09-24 15:29:48 -0400

    Tentatively solve problem 31

---
+++ ProjectEuler/Problems/Problem_0031.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjectEuler.Problems;
+
+internal class Problem_0031 : Problem
+{
+	public int N = 20;
+
+	public static List<int> coins = [1, 2, 5, 10, 20, 50, 100, 200];
+
+	/// <returns>The number of different ways N pence can be made using any number of coins.</returns>
+	public override object Solve()
+	{
+		coins.Reverse();
+		return GetWays(N, coins);
+	}
+
+	public static int GetWays(int n, IEnumerable<int> units)
+	{
+		if (n == 0) return 0;
+
+		var count = 0;
+		units = units.Where(x => x <= n);
+		foreach (var u in units)
+		{
+			if (u == n) count++;
+			else count += GetWays(n - u, units.Where(x => x <= u));
+		}
+		return count;
+	}
+}
+
