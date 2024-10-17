875833daab9d129564263e32336f64f54e234bec
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-10-17 14:12:58 -0400

    Clean up problem 31

--- ProjectEuler/Problems/Problem_0031.cs
+++ ProjectEuler/Problems/Problem_0031.cs
@@ -3,7 +3,7 @@
 internal class Problem_0031 : Problem
 {
 	public int N = 200;
-	public static List<int> coins = [1, 2, 5, 10, 20, 50, 100, 200];
+	public List<int> coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
 	/// <returns>The number of different ways N pence can be made using any number of coins.</returns>
 	public override object Solve()
@@ -12,19 +12,16 @@ internal class Problem_0031 : Problem
 		return GetWays(N, coins, []);
 	}
 
-	public static int GetWays(
-		int n,
-		IEnumerable<int> units,
-		Dictionary<(int, int), int> cache)
+	public static int GetWays(int n, List<int> units, Dictionary<(int, int), int> cache)
 	{
 		if (n == 0) return 0;
 		if (cache.TryGetValue((n, units.First()), out var value)) return value;
 
 		var count = 0;
-		units = units.Where(x => x <= n);
-		foreach (var u in units)
-			if (u == n) count++;
-			else count += GetWays(n - u, units.Where(x => x <= u), cache);
+		units = units.Where(x => x <= n).ToList();
+		for (var i = 0; i < units.Count; i++)
+			if (units[i] == n) count++;
+			else count += GetWays(n - units[i], units[i..], cache);
 
 		cache[(n, units.First())] = count;
 		return count;
