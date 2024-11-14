c2a41f67af00a85db5e95f1d34cd8ed3a5c7af6f
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-11-14 10:05:29 -0500

    Use ArraySegment in GetSlidingWindows

--- ProjectEuler/Utils.cs
+++ ProjectEuler/Utils.cs
@@ -5,12 +5,11 @@ namespace ProjectEuler;
 public static class Utils
 {
 	/// <returns>A sequence that contains each consecutive sliding window within the source.</returns>
-	public static IEnumerable<T[]> GetSlidingWindows<T>(this T[] source, int size)
+	public static IEnumerable<ArraySegment<T>> GetSlidingWindows<T>(this T[] source, int size)
 	{
-		// TODO: Add direction parameter
 		for (var i = 0; i <= source.Length - size; i++)
 		{
-			yield return source[i..(i + size)];
+			yield return new ArraySegment<T>(source, i, size);
 		}
 	}
 
