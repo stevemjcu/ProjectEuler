988f05ada05977261bea955ee2892a5391b447d7
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-11-24 14:14:07 -0500

    Clean up solutions 58 and 59

--- ProjectEuler/Problems/Problem_0058.cs
+++ ProjectEuler/Problems/Problem_0058.cs
@@ -3,16 +3,13 @@
 public class Problem_0058 : Problem
 {
 	public double N = 0.1;
-	public int M = 10000;
 
-	/// <returns>
-	/// The side length of the square spiral where the ratio of primes on both diagonals falls under N.
-	/// </returns>
+	/// <returns>The side length of the square spiral where the ratio of diagonal primes falls under N.</returns>
 	public override object Solve()
 	{
 		var ratios = ExpandPrimeRatios(ExpandSpiralDiagonals());
-		var idx = ratios.Skip(1).TakeWhile(r => r >= N).Count();
-		return (idx + 1) * 2 + 1; // skipped first layer (0%)
+		var idx = ratios.Skip(1).TakeWhile(r => r >= N).Count() + 1; // skip first layer (0:1)
+		return idx * 2 + 1;
 	}
 
 	// consider layers l = 0..N
@@ -24,32 +21,32 @@ public class Problem_0058 : Problem
 	/// <returns>The sequence of diagonal numbers on each layer of a square spiral.</returns>
 	private static IEnumerable<int[]> ExpandSpiralDiagonals()
 	{
-		var cur = 1;
-		yield return [cur];
+		var itr = 1;
+		yield return [itr];
 		for (var i = 1; ; i++)
 		{
-			var arr = new int[4];
+			var layer = new int[4];
 			for (var j = 0; j < 4; j++)
 			{
-				cur += i * 2;
-				arr[j] = cur;
+				itr += i * 2;
+				layer[j] = itr;
 			}
-			yield return arr;
+			yield return layer;
 		}
 	}
 
 	/// <returns>The sequence of aggregate ratios of diagonal primes on each layer of a square spiral.</returns>
 	private static IEnumerable<double> ExpandPrimeRatios(IEnumerable<int[]> spiral)
 	{
-		var i = 0;
+		var idx = 0;
 		var length = 0;
 		var primes = 0;
 		foreach (var layer in spiral)
 		{
-			length += i == 0 ? 1 : 4;
+			length += idx == 0 ? 1 : 4;
 			primes += layer.Count(Utils.IsPrime);
 			yield return (double)primes / length;
-			i++;
+			idx++;
 		}
 	}
 }
--- ProjectEuler/Problems/Problem_0059.cs
+++ ProjectEuler/Problems/Problem_0059.cs
@@ -3,7 +3,6 @@
 public class Problem_0059 : Problem
 {
 	public int N = 3;
-	private static readonly string[] _words = ["that", "with", "this"];
 
 	/// <returns>
 	/// The sum of the decrypted ASCII values in the encrypted cipher, knowing that the encryption 
@@ -11,47 +10,17 @@ public class Problem_0059 : Problem
 	/// </returns>
 	public override object Solve()
 	{
-		// Enumerate possible encryption keys
-		// Select decrypted messages
-		// Find message with common english words (?) <- split by spaces and look for words like "the"?
-		// Summate message
-
-		// each letter can be a..z (27 possibilities)
-		// repeats are permissible, e.g., aaa
-
-		// length n = 3
-		// base b = 2
-		// permutations p = 2^3 = 8
-
-		// 000
-		// 001
-		// 010
-		// 011
-		// 100
-		// 101
-		// 110
-		// 111
-
-		// so if b = 27, p = 27^3 = 19,683 possible keys
-		// permutations(abc) = permutations(a) x permutations(bc)
-		// permutations(bc) = permtuations(b) x permutations(c)
-
 		var cipher = Resource
 			.Split(",")
 			.Select(i => (char)int.Parse(i))
 			.ToArray();
 
-		foreach (var key in GenerateKeys(N))
-		{
-			var message = Decrypt(new(cipher), key);
-			if (message.Split(" ").Any(word => _words.Contains(word)))
-				return message.Select(c => (int)c).Sum();
-		}
-		return 0;
+		var message = Decrypt(new(cipher), GenerateKeys(N));
+		return message.Select(c => (int)c).Sum();
 	}
 
-	/// <returns>The sequence of possible n-length lower case encryption keys.</returns>
-	public static IEnumerable<string> GenerateKeys(int n)
+	/// <returns>The sequence of 27^n n-length lower case encryption keys.</returns>
+	private static IEnumerable<string> GenerateKeys(int n)
 	{
 		if (n == 1)
 			for (var i = 0; i < 27; i++)
@@ -62,8 +31,26 @@ public class Problem_0059 : Problem
 					yield return new([.. a, .. b]);
 	}
 
+	/// <returns>The decrypted message produced from the given cipher and keys.</returns>
+	private static string Decrypt(string cipher, IEnumerable<string> keys)
+	{
+		//foreach (var key in keys)
+		//{
+		//	var message = Decrypt(new(cipher), key);
+		//	if (message
+		//		.Split(" ")
+		//		.Any(word => ["that", "with", "this"].Contains(word)))
+		//		return message;
+		//}
+		//return string.Empty;
+
+		// Worse best case but same worst case and better guarantee?
+		var key = keys.MaxBy(k => Decrypt(cipher, k).Count(c => c == ' '));
+		return key == null ? string.Empty : Decrypt(cipher, key);
+	}
+
 	/// <returns>The decrypted message produced from the given cipher and key.</returns>
-	public static string Decrypt(string cipher, string key)
+	private static string Decrypt(string cipher, string key)
 	{
 		var message = new char[cipher.Length];
 		for (var i = 0; i < cipher.Length; i++)
